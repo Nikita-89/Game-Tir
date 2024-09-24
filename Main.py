aedc673a9daa03dbe2a2f4740459c0d84f930c4d
Nikita-89 <nikita1989188@mail.ru>
2024-09-24 15:55:44 +0300

    For lesson (delete)

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import time
 pygame.init()
 
 # Настройки окна
-SCREEN_WIDTH = 800
+
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
